67fea556e206fc77a1816e3976bb17a4510a78aa
Ian Wright <ian.wright@well.ox.ac.uk>
2013-04-09 09:25:04 +0100

    Download docx parser

--- pf-internal-db/setup.sh
+++ pf-internal-db/setup.sh
@@ -5,3 +5,6 @@ apt-get install libmysqlclient-dev
 apt-get install python-dev
 apt-get install unzip
 pip install distribute --upgrade
+curl -sL http://github.com/mikemaccana/python-docx/tarball/master > python-docx.tar.gz
+tar zxf python-docx.tar.gz 
+apt-get install libxml2-dev libxslt-dev
