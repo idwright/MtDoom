a626e090ddf4a6015b07c6d5998b883f64cadcc2
Ian Wright <ian.wright@well.ox.ac.uk>
2013-04-26 15:17:19 +0100

    Get config from home instead of .

--- pf-internal-db/setup_popgen.sh
+++ pf-internal-db/setup_popgen.sh
@@ -3,7 +3,7 @@ DQXIteration=It34
 FILE=${Iteration}.tar.gz
 DIR=PfPopGenWeb-${Iteration}
 DATABASE=pfpopgen
-. ./config.sh
+. ~/config.sh
 curl -sL --user "${username}:${password}" https://github.com/malariagen/PfPopGenWeb/archive/${FILE} > ${FILE}
 tar xf ${FILE}
 cd ${DIR}
